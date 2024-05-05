596666268f6e3c43c73b7d51b3906ae76ae08d58
Neha <146248620+nehag2005@users.noreply.github.com>
2024-05-05 20:10:12 +0100

    countdown

---
+++ index.js
@@ -0,0 +1,54 @@
+// Countdown timer
+
+let day = document.getElementById("days");
+let hour = document.getElementById("hours");
+let minute = document.getElementById("minutes");
+let second = document.getElementById("seconds");
+
+// Format - Date(yr, mon, day, hr, min)
+
+let eventDate = new Date(2024, 6, 6, 18, 30); // Sat Jul 06 2024 18:30:00
+let eventTime = eventDate.getTime();
+
+
+
+function timer() {
+
+    
+    let todayDate = new Date();
+    // time in milliseconds
+    let todayTime = todayDate.getTime();
+
+    let subtractedTime = eventTime - todayTime;
+
+    // 60 seconds = 1000ms, 1hr = 60 min...
+
+    let oneMinute = 60 * 1000;
+    let oneHour = 60 * oneMinute;
+    let oneDay= 24 * oneHour;
+
+
+    // single digit num 
+    let addZeroes = num => num < 10 ? `0${num}` : num;
+
+    if (eventTime < todayTime) {
+        clearInterval(interval);
+        document.querySelector(".countdown").innerHTML = 
+        `<h1>It's Event Time!</h1>`;
+    } else {
+        let daysRemaining = Math.floor(subtractedTime / oneDay);
+        let hoursRemaining = Math.floor((subtractedTime % oneDay) / oneHour);
+        let minutesRemaining = Math.floor((subtractedTime % oneHour) / oneMinute);
+        let secondsRemaining = Math.floor((subtractedTime % oneMinute) / 1000);
+
+        day.textContent = addZeroes(daysRemaining);
+        hour.textContent = addZeroes(hoursRemaining);
+        minute.textContent = addZeroes(minutesRemaining);
+        second.textContent = addZeroes(secondsRemaining);   
+
+    }
+
+}
+
+let interval = setInterval(timer, 1000);
+timer();
